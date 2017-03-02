105bd8b3b33570354894c2dec9f2665a0e67fa7e
Miles Grandin <miles.grandin@gmail.com>
2017-03-02 16:51:54 -0700

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+-------------------------+" );
+    System.out.println( "|                         |" );
+    System.out.println( "|       CORNER STORE      |" );
+    System.out.println( "|                         |" );
+    System.out.println( "|   2017-03-02  04:39PM   |" );
+    System.out.println( "|                         |" );
+    System.out.println( "|  Gallons:       10.870  |" );
+    System.out.println( "|  Price/gallon:  $2.148  |" );
+    System.out.println( "|                         |" );
+    System.out.println( "|  Fuel Total:   $23.35   |" );
+    System.out.println( "|                         |" );
+    System.out.println( "+-------------------------+" );
+  }
+}
