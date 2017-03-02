6db67ff4de7641ef99e1a272252580a8a658caf2
Miles Grandin <miles.grandin@gmail.com>
2017-03-02 16:49:59 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Miles Grandin, am determined to learn how to code." );
+    System.out.println( "Today's date is Thrusday, March 2, 2017" );
+  }
+}
