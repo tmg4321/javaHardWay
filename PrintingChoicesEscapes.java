0dc391baaea0ad1fa115932476455e1d23cee9fa
Miles Grandin <miles.grandin@gmail.com>
2017-03-03 10:36:49 -0700

    add PrintingChoicesEscapes

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,5 @@
+public class PrintingChoicesEscapes {
+  public static void main( String[] args ) {
+    System.out.println( "\nAlpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\nThis is a test.\n\nI am learning Java the Hard Way!\n");
+  }
+}
