4977671f5550c5e32d40233d538cb7a807f54bc4
Miles Grandin <miles.grandin@gmail.com>
2017-03-07 12:59:16 -0700

    add exercise 29 while

---
+++ CoinFlip2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CoinFlip2 {
+  public static void main( String[] args ) {
+    Scanner keyboard = new Scanner(System.in);
+    String coin, again = "y";
+    int streak = 0;
+    boolean gotHeads;
+
+    while ( again.equals("y") ) {
+
+      gotHeads = Math.random() < 0.5;
+
+      if ( gotHeads )
+        coin = "HEADS";
+      else
+        coin = "TAILS";
+
+      System.out.println("\nYou flip a coin and it is ..." + coin);
+
+      if ( gotHeads ) {
+        streak++;
+        System.out.println("\n\tThat's " + streak + " in a row...");
+        System.out.print("\tWould you like to flip again? (y/n): ");
+        again = keyboard.next();
+      }
+      else {
+        System.out.println("\tYou lose everything!");
+        System.out.println("\tShould've quit while you were aHEAD...");
+        streak = 0;
+        again = "n";
+      }
+    }
+    System.out.println("\nFinal score: " + streak + "\n" );
+  }
+}
