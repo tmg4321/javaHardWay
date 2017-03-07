58322aaa890751466d62704f9177920e004f4153
Miles Grandin <miles.grandin@gmail.com>
2017-03-07 10:00:33 -0700

    add exercise 26

---
+++ HighLow.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HighLow {
+  public static void main( String[] args ) {
+    Scanner keyboard = new Scanner(System.in);
+    int secret, guess;
+
+    secret = 1 + (int)( 100*Math.random() );
+
+    System.out.println( "\nI'm thinking of a number between 1-100." );
+    System.out.println( "Try to guess it!" );
+    System.out.print( "> " );
+    guess = keyboard.nextInt();
+
+    while ( secret != guess ) {
+      if ( guess < secret ) {
+        System.out.println( "\nSorry, your guess is low." );
+      }
+      if ( guess > secret ) {
+        System.out.println( "\nSorry, your guess is high." );
+      }
+        System.out.print( "Try again. \n>" );
+        guess = keyboard.nextInt();
+    }
+
+    System.out.println( "\nYou guessed it! What are the odds?!?\n" );
+  }
+}
